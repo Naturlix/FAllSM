de338b00f10cd241a003726482898450644a6da9
Naturlix <INaturlix@gmail.com>
2015-05-09 21:45:19 +0300

    Create cubefunction.h

---
+++ cubefunction.h
@@ -0,0 +1,16 @@
+#define resy	768
+#define resx	1366
+#define PPM	30.0f
+#define MPP	(1.0f/PPM)
+#define COMPLEXITY 50 //cube creating time
+const float SCALE = 30.f;
+const float density = 1.f;
+const float friction = 0.7f;
+const float g=9.8f;
+
+
+
+//cubes list----------------------------------------
+#define funcsum 2 //number of cube examples
+std::string name_list[funcsum]={"dev.png","mov.png"};
+//--------------------------------------------------
